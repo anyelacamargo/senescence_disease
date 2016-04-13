88a2d26d26f03ec7553a333a7ae9b9f6384670f8
anyelacamargo <acamargo@rocketmail.com>
2016-04-13 20:06:54 +0100

    Added analytics

--- analytics_genetics.R
+++ analytics_genetics.R
@@ -1,7 +1,37 @@
 # This script does QTL mapping 
 rm(list=ls()); # Delete files
 cat("\014");
-library(mpMap)
+#library(mpMap)
+
+runmpMap = function()
+{
+  #mpMap data
+  # 
+  # nind=720;
+  # pedm=cbind(1: (8+nind), rep(0, 8+nind), rep(0, 8+nind), c(rep(0, 8), rep(1, nind)));
+  # 
+  # write.table(rils[1:10,], file='rils1.csv', sep=',', row.names = FALSE);
+  # write.table(founders, file='founders.csv', sep=',', row.names = FALSE );
+  # write.table(ped, file='ped.csv', sep=',', row.names = FALSE);
+  # write.table(rils, file='rils.csv', sep=',', row.names = FALSE);
+  # 
+  # break();
+  # rils = read.table('rils.csv', header=T,sep=',');
+  # founders = read.table('founders.csv', header=T,sep=',');
+  # ped = read.table('ped.csv', header=T, sep=',');
+  # mapdata = read.table('wheat_geno_coordinates.csv', header=TRUE, sep=',');
+  # 
+  # 
+  # o <- mpcross(finals=rils, founders=founders, fid=levels(founders$id),  id=levels(rils$id), 
+  #                  pedigree=pedm);
+  # 
+  # mpprob(o, program='happy');
+  # o1 <- read.mpcross(finalfile='rils.csv', founderfile='founders.csv', pedfile='ped.csv', 
+  #                   phenofile='wheat_pheno.csv', mapfile = 'wheat_geno_coordinates.csv');
+  # 
+  # 
+}
+
 
 
 # Convert SNPs
@@ -23,25 +53,54 @@ convert.snp2 <- function(x) {
   y <- rep(NA,length(x));
   #print(alleles);
   y[which(x==alleles[1])] <- 0;
-  y[which(x==alleles[2])] <- 1;
-  y[which(x==alleles[3])] <- 2;
+  y[which(x==alleles[2])] <- 2;
+  y[which(x==alleles[3])] <- 1;
   #break();
   return(y)
 }
+
+searchPolym = function(data)
+{
+  
+  profile = data;
+  #print(profile)
+  a0 <- as.numeric(profile <= 1);
+  a1 <- as.numeric(profile >= 1);
+  if(mean(a0) == 1 | mean(a1) == 1) { return(0)}
+  else {return(1)};
+}
+
 
 # phenodata
 phenodata = read.table('wheat_pheno.csv', header=TRUE, sep=',');
 # Founders
 founders_raw = read.table('founders.geno.csv', header=T, sep=',');
+#rils
+rils_raw = read.table('wheat_geno.csv', header=T, sep=',');
+
+# Process founders
 colnames(founders_raw);
 clf = ncol(founders_raw); 
 rf = nrow(founders_raw);# Cols dataframe
+
+# Search for markers with non-polymorphic alleles
+flag=as.numeric(sapply(rownames(founders_raw), 
+            function(x) searchPolym(as.numeric(founders_raw[x,9:clf]))));
+i = which(flag == 0);
+# List of non-poly alleles
+nonpolymarkers = founders_raw$X13074[i];
+
+
+# Convert 
+founders_raw = founders_raw[-i, ];
 F <- apply(founders_raw[,9:clf],2, convert.snp1);
 founders = data.frame(colnames(founders_raw)[9:clf], t(F));
 colnames(founders) = c('genotype', as.vector(founders_raw$X));
 
 # Rils
-rils_raw = read.table('wheat_geno.csv', header=T, sep=',');
+# delete poly markers
+o = match(nonpolymarkers,rils_raw$original.order);
+rils_raw = rils_raw[-o,];
 clr = ncol(rils_raw); 
 rr = nrow(rils_raw);# Cols dataframe
 R <- apply(rils_raw[3:rr,3:clr],2, convert.snp2);
@@ -49,30 +108,6 @@ v = as.vector(rils_raw[2, 3:clr]);
 rils = data.frame(t(v), t(R));
 colnames(rils) = c('genotype', as.vector(rils_raw$original.order[3:rr]));
 
-#Map data
-mapdata = read.table('wheat_geno_coordinates.csv', header=TRUE, sep=',');
-
-
-nind=720;
-pedm=cbind(1: (8+nind), rep(0, 8+nind), rep(0, 8+nind), c(rep(0, 8), rep(1, nind)));
-
-write.table(rils[1:10,], file='rils1.csv', sep=',', row.names = FALSE);
-write.table(founders, file='founders.csv', sep=',', row.names = FALSE );
-write.table(ped, file='ped.csv', sep=',', row.names = FALSE);
-write.table(rils, file='rils.csv', sep=',', row.names = FALSE);
-
-break();
-rils = read.table('rils.csv', header=T,sep=',');
-founders = read.table('founders.csv', header=T,sep=',');
-ped = read.table('ped.csv', header=T, sep=',');
-
-o <- mpcross(finals=rils, founders=founders, fid=levels(founders$id),  id=levels(rils$id), 
-                 pedigree=pedm);
-
-mpprob(o, program='happy');
-o1 <- read.mpcross(finalfile='rils.csv', founderfile='founders.csv', pedfile='ped.csv', 
-                  phenofile='wheat_pheno.csv', mapfile = 'wheat_geno_coordinates.csv');
-
-
-
+rildata = merge(phenodata[,c(1,17)], rils, by.x='genotype', by.y='genotype');
+write.table(rildata, file='rildisease.ped', sep=' ', quotes=F, row.names = FALSE);
 
