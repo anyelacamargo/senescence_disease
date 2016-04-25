b9bb63999582b88d78d107068f70a2bae6a2dc86
acamargo <acamargo@rocketmail.com>
2016-04-25 17:57:26 +0100

    New changed to paper

--- analytics_genetics.R
+++ analytics_genetics.R
@@ -5,37 +5,6 @@ cat("\014");
 source('genohack.R');
 #library(mpMap)
 
-runmpMap = function()
-{
-  #mpMap data
-  # 
-  # nind=720;
-  # pedm=cbind(1: (8+nind), rep(0, 8+nind), rep(0, 8+nind), c(rep(0, 8), rep(1, nind)));
-  # 
-  # write.table(rils[1:10,], file='rils1.csv', sep=',', row.names = FALSE);
-  # write.table(founders, file='founders.csv', sep=',', row.names = FALSE );
-  # write.table(ped, file='ped.csv', sep=',', row.names = FALSE);
-  # write.table(rils, file='rils.csv', sep=',', row.names = FALSE);
-  # 
-  # break();
-  # rils = read.table('rils.csv', header=T,sep=',');
-  # founders = read.table('founders.csv', header=T,sep=',');
-  # ped = read.table('ped.csv', header=T, sep=',');
-  # mapdata = read.table('wheat_geno_coordinates.csv', header=TRUE, sep=',');
-  # 
-  # 
-  # o <- mpcross(finals=rils, founders=founders, fid=levels(founders$id),  id=levels(rils$id), 
-  #                  pedigree=pedm);
-  # 
-  # mpprob(o, program='happy');
-  # o1 <- read.mpcross(finalfile='rils.csv', founderfile='founders.csv', pedfile='ped.csv', 
-  #                   phenofile='wheat_pheno.csv', mapfile = 'wheat_geno_coordinates.csv');
-  # 
-  # 
-}
-
-
-
 # Convert SNPs
 convert.snp1 <- function(x) {
   #convert to {-1,0,1,NA}
@@ -55,8 +24,8 @@ convert.snp2 <- function(x) {
   y <- rep(NA,length(x));
   #print(alleles);
   y[which(x==alleles[1])] <- 0;
-  y[which(x==alleles[2])] <- 2;
-  y[which(x==alleles[3])] <- 1;
+  y[which(x==alleles[2])] <- 1;
+  y[which(x==alleles[3])] <- 0;
   #break();
   return(y)
 }
@@ -74,6 +43,62 @@ searchPolym = function(data)
 }
 
 
+process_founders = function(datafounder, datamap, kw){
+
+  
+  colnames(datafounder);
+  clf = ncol(datafounder); 
+  rf = nrow(datafounder);# Cols dataframe
+  # Search for markers with non-polymorphic alleles
+  flag=as.numeric(sapply(rownames(datafounder), 
+                         function(x) searchPolym(as.numeric(datafounder[x,9:clf]))));
+  i = which(flag == 0);
+  # List of non-poly alleles
+  nonpolymarkers = datafounder$X13074[i];
+  
+  # Convert 
+  datafounder = datafounder[-i, ];
+  F <- apply(datafounder[,9:clf],2, convert.snp1);
+  founders = data.frame(colnames(datafounder)[9:clf], t(F));
+  colnames(founders) = c('genotype', as.vector(datafounder$X));
+  mapfounder = merge(map_raw, datafounder, by.x = 'Marker', by.y=kw);
+  return(mapfounder);
+  
+}
+
+process_rils = function(data, map_raw)
+{
+  rils  = data;  
+  colnames(rils);
+  rils_raw_clean = merge(mapfounder[,1:2], rils, by.x='Marker', by.y='original.order');
+  clr = ncol(rils_raw_clean); 
+  rr = nrow(rils_raw_clean);# Cols dataframe
+  R <- apply(rils_raw_clean[,4:clr],2, convert.snp2);
+  v = as.vector(rils[2, 3:(clr-1)]);
+  rils = data.frame(t(v), t(R));
+  colnames(rils) = c('genotype', as.vector(rils_raw_clean$Marker));
+  return(rils);
+  
+}
+
+create_phenodata = function(phenotable, riltable)
+{
+  c = ncol(riltable);
+  g = t(riltable[1:2,3:c]);
+  k = merge(g, phenotable, by.x='2', by.y='genotype');
+  k = k[,-1];
+  colnames(k)[1] = 'SUBJECT.NAME';
+  return(k);
+}
+
+create_pedfile = function()
+{
+  
+  rildata = merge(phenodata[,c(1,17)], rils, by.x='genotype', by.y='genotype');
+  write.table(rildata, file='rildisease.ped', sep=' ', quote = FALSE, row.names = FALSE, col.names = FALSE);
+  write.table(rils, file='rildisease1.ped', sep=' ', quote = FALSE, row.names = FALSE, col.names = FALSE);
+}
+
 # phenodata
 phenodata = read.table('wheat_pheno.csv', header=TRUE, sep=',');
 # Founders
@@ -82,51 +107,15 @@ founders_raw = read.table('founders.geno.csv', header=T, sep=',');
 rils_raw = read.table('wheat_geno.csv', header=T, sep=',');
 # map
 map_raw = read.table('wheat_geno_coordinates.csv', sep=',', header=TRUE);
-
-
-# Process founders
-colnames(founders_raw);
-clf = ncol(founders_raw); 
-rf = nrow(founders_raw);# Cols dataframe
-
-# Search for markers with non-polymorphic alleles
-flag=as.numeric(sapply(rownames(founders_raw), 
-            function(x) searchPolym(as.numeric(founders_raw[x,9:clf]))));
-i = which(flag == 0);
-# List of non-poly alleles
-nonpolymarkers = founders_raw$X13074[i];
-
-
-
-# Convert 
-founders_raw = founders_raw[-i, ];
-F <- apply(founders_raw[,9:clf],2, convert.snp1);
-founders = data.frame(colnames(founders_raw)[9:clf], t(F));
-colnames(founders) = c('genotype', as.vector(founders_raw$X));
-
 # map x founder
+mapfounder = process_founders(founders_raw, map_raw, 'X13074');
 
-mapfounder = merge(map_raw, founders_raw, by.x = 'Marker', by.y='X13074');
+# Process Rils
+#rils = process_rils(rils_raw);
 
-# Rils
-# delete poly markers
-colnames(rils_raw);
-rils_raw_clean = merge(mapfounder[,1:2], rils_raw, by.x='Marker', by.y='original.order');
-clr = ncol(rils_raw_clean); 
-rr = nrow(rils_raw_clean);# Cols dataframe
-R <- apply(rils_raw_clean[,4:clr],2, convert.snp2);
-v = as.vector(rils_raw[2, 3:(clr-1)]);
-rils = data.frame(t(v), t(R));
-colnames(rils) = c('genotype', as.vector(rils_raw_clean$Marker));
-
-rildata = merge(phenodata[,c(1,17)], rils, by.x='genotype', by.y='genotype');
-write.table(rildata, file='rildisease.ped', sep=' ', quote = FALSE, row.names = FALSE, col.names = FALSE);
-write.table(rils, file='rildisease1.ped', sep=' ', quote = FALSE, row.names = FALSE, col.names = FALSE);
+#create allele file
 g2a(mapfounder, ".alleles");
 
-c = ncol(rils_raw);
-g = t(rils_raw[1:2,3:c]);
-k = merge(g, phenodata[,c(1, 17)], by.x='2', by.y='genotype');
-k = k[,-1];
-colnames(k)[1] = 'SUBJECT.NAME';
-write.table(k, file='wheat.phenotype', sep='\t', quote = FALSE, row.names = FALSE);
+#create phenofile
+phenodata = create_phenodata(phenodata, rils_raw);
+write.table(phenodata,file='wheat.phenotype', sep='\t', quote = FALSE, row.names = FALSE);
