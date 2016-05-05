eadf78028bb67f8e5499216ead17a5a1668c5336
acamargo <acamargo@rocketmail.com>
2016-05-05 15:57:42 +0100

    A file has been added. Tex and R script have been updated

--- analytics_genetics.R
+++ analytics_genetics.R
@@ -4,6 +4,7 @@ rm(list=ls()); # Delete files
 cat("\014");
 source('genohack.R');
 #library(mpMap)
+library(xtable)
 
 # Convert SNPs
 convert.snp1 <- function(x) {
@@ -114,7 +115,7 @@ get_MAGIC = function(phenofile, foundersgenofile, rildgenofile, mapfile)
   f$map_raw = read.table(mapfile, sep=',', header=TRUE);
   
   f$founders_raw = merge(f$map_raw[,1:2], f$founders_raw, by.y='X13074', by.x='Marker');
-  f$founders_raw = founders_raw[,c(3,4,2,6:8,1, 9:17)];
+  f$founders_raw = f$founders_raw[,c(3,4,2,6:8,1, 9:17)];
   colnames(f$founders_raw)[3] = 'pos';
   colnames(f$founders_raw)[7] = 'X13074';
   
@@ -125,11 +126,24 @@ get_MAGIC = function(phenofile, foundersgenofile, rildgenofile, mapfile)
   f$mapfounder = data.frame(f$mapfounder, chromosome= sapply(f$mapfounder$Chr, function(x) paste('chr', x, sep='')));
   f$mapfounder = f$mapfounder[,c(1,20,3,2, 6:19)];
   write.table(f$mapfounder[,c(1:3)],file='map.wheat.txt', sep='\t', quote = FALSE, row.names = FALSE);
-  write.table(f$phenodata,file='wheat.phenotype', sep='\t', quote = FALSE, row.names = FALSE);
+  write.table(f$phenodata, file='wheat.phenotype', sep='\t', quote = FALSE, row.names = FALSE);
   write.table(f$founders_raw,file='founders.geno2.txt', sep=' ', quote = FALSE, row.names = FALSE);
   #g2a(f$mapfounder, ".wheat.alleles");
   return(f);
 }
+
+createQTLTable = function()
+{
+  files <- list.files(pattern = "qtls");
+  myfiles = do.call(rbind, lapply(files, 
+                                  function(x) read.table(x, stringsAsFactors = TRUE, header=T)));
+  
+  print(xtable(myfiles[,c(-3:-5)]), 
+        type='latex', file='qtltable.tex')
+  
+  
+}
+
 
 
 f = get_MAGIC('wheat_pheno.csv', 'founders.geno.csv','wheat_geno.csv','wheat_geno_coordinates.csv'); 
