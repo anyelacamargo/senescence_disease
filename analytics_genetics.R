0a6ffb3da9cb17895aaf060ff861fdf908d747a4
acamargo <acamargo@rocketmail.com>
2016-06-23 10:22:43 +0100

    new files

--- analytics_genetics.R
+++ analytics_genetics.R
@@ -3,23 +3,21 @@
 rm(list=ls()); # Delete files
 cat("\014");
 source('genohack.R');
-#library(mpMap)
-library(xtable)
+library('mpMap')
+library(rrBLUP)
 
-createQTLTable = function(mapdata)
-{
-  
-  files <- list.files(pattern = "qtls");
-  myfiles = do.call(rbind, lapply(files, 
-                                  function(x) read.table(x, stringsAsFactors = TRUE, header=T)));
-  
-  h = merge(myfiles, mapdata, by.x='peak.SNP', by.y='Marker');
-  h = h[order(h$phenotype,h$chromosome),];
-  h$logP = round(h$logP,2);
-  h$genomewide.pvalue = round(h$genomewide.pvalue,2);
-  h$cM = round(h$cM,2);
-  return(h);
+# Convert SNPs
+convert.snp <- function(x) {
+  #convert to {-1,0,1,NA}
+  alleles <- c(0, 2, 1); # 0=AA, 2=BB
+  y <- rep(NA,length(x));
+  #print(alleles);
+  y[which(x==alleles[1])] <- 'AA';
+  y[which(x==alleles[2])] <- 'BB';
+  y[which(x==alleles[3])] <- 'AB';
+  return(y)
 }
+
 
 # Convert SNPs
 convert.snp1 <- function(x) {
@@ -59,8 +57,15 @@ searchPolym = function(data)
 }
 
 
-process_founders = function(datafounder, mapdata, kw){
-
+process_founders = function(datafounder, mapdata, kw)
+{
+  datafounder= founders_raw
+  mapdata = map_raw
+  f = list();
+  datafounder = merge(mapdata[,1:2], datafounder, by.y='X13074', by.x='Marker');
+  datafounder = datafounder[,c(3,4,2,6:8,1, 9:17)];
+  colnames(datafounder)[3] = 'pos';
+  colnames(datafounder)[7] = 'X13074';
   
   colnames(datafounder);
   clf = ncol(datafounder); 
@@ -75,25 +80,41 @@ process_founders = function(datafounder, mapdata, kw){
   # Convert 
   datafounder = datafounder[-i, ];
   F <- apply(datafounder[,9:clf],2, convert.snp1);
-  founders = data.frame(colnames(datafounder)[9:clf], t(F));
-  colnames(founders) = c('genotype', as.vector(datafounder$X));
-  mapfounder = merge(mapdata, datafounder, by.x = 'Marker', by.y=kw);
-  return(mapfounder);
+  foundersV = data.frame(datafounder$X13074, F);
+  colnames(foundersV)[1] = 'Marker';
+  f$foundersH = data.frame(colnames(datafounder)[9:clf], t(F));
+  colnames(f$foundersH) = c('genotype', as.vector(datafounder$X));
+  f$mapfounder = merge(mapdata, foundersV, by.x = 'Marker', by.y='Marker');
+  f$mapfounder = data.frame(f$mapfounder, 
+                            chromosome= sapply(f$mapfounder$Chr, function(x) paste('chr', x, sep='')));
+  return(f);
   
 }
 
-process_rils = function(data, map_raw)
+process_rils = function(dataril, datamap)
 {
-  rils  = data;  
-  colnames(rils);
-  rils_raw_clean = merge(mapfounder[,1:2], rils, by.x='Marker', by.y='original.order');
+  f = list()
+  rils = dataril
+  #rils = rils_raw
+  #datamap=map_raw
+  
+  
+  rils_raw_clean = merge(datamap[,1:2], rils, by.x='Marker', by.y='original.order');
   clr = ncol(rils_raw_clean); 
   rr = nrow(rils_raw_clean);# Cols dataframe
-  R <- apply(rils_raw_clean[,4:clr],2, convert.snp2);
-  v = as.vector(rils[2, 3:(clr-1)]);
-  rils = data.frame(t(v), t(R));
-  colnames(rils) = c('genotype', as.vector(rils_raw_clean$Marker));
-  return(rils);
+  f$R <- apply(rils_raw_clean[,4:clr],2, convert.snp2);
+  #v = as.vector(rils_raw_clean[2, 3:(clr-1)]);
+  markername = as.vector(rils_raw_clean[1:rr, 'Marker']);
+  genotypename = as.vector(rils[1, 3:(clr-1)]);
+  genotypename = unlist(t(genotypename));
+  
+  f$rildataV = data.frame(markername, f$R);
+  colnames(f$rildataV) = c('Marker', genotypename);
+  rownames(f$rildataV) = 1:nrow(f$rildataV);
+  f$rildataH = data.frame(genotypename, t(f$R));
+  colnames(f$rildataH) = c('Marker', markername);
+  rownames(f$rildataH) = 1:nrow(f$rildataH);
+  return(f);
   
 }
 
@@ -117,30 +138,21 @@ create_pedfile = function()
 }
 
 
+check_PStruct = function(data)
+{
+  
+  A <- A.mat(data);
+  eig.result <- eigen(A)
+  lambda <- eig.result$values
+  return(lambda);
+ 
+}
+
+
 get_MAGIC = function(phenofile, foundersgenofile, rildgenofile, mapfile)
 {
   #phenodata
   f = list()
-  f$pheno_raw = read.table(phenofile, header=TRUE, sep=',');
-  # foundersdata
-  f$founders_raw = read.table(foundersgenofile, header=T, sep=',');
-  #rilsdata
-  f$rils_raw = read.table(rildgenofile, header=T, sep=',');
-  # map
-  f$map_raw = read.table(mapfile, sep=',', header=TRUE);
-  
-  f$founders_raw = merge(f$map_raw[,1:2], f$founders_raw, by.y='X13074', by.x='Marker');
-  f$founders_raw = f$founders_raw[,c(3,4,2,6:8,1, 9:17)];
-  colnames(f$founders_raw)[3] = 'pos';
-  colnames(f$founders_raw)[7] = 'X13074';
-  
-  # map x founder
-  f$mapfounder = process_founders(f$founders_raw, f$map_raw, 'X13074');
-    #create allele file
-  f$phenodata = create_phenodata(f$pheno_raw, f$rils_raw);
-  f$mapfounder = data.frame(f$mapfounder, chromosome= sapply(f$mapfounder$Chr, function(x) paste('chr', x, sep='')));
-  f$mapfounder = f$mapfounder[,c(1,20,3,2, 6:19)];
-  write.table(f$mapfounder[,c(1:3)],file='map.wheat.txt', sep='\t', quote = FALSE, row.names = FALSE);
   write.table(f$phenodata, file='wheat.phenotype', sep='\t', quote = FALSE, row.names = FALSE);
   write.table(f$founders_raw,file='founders.geno2.txt', sep=' ', quote = FALSE, row.names = FALSE);
   #g2a(f$mapfounder, ".wheat.alleles");
@@ -148,10 +160,104 @@ get_MAGIC = function(phenofile, foundersgenofile, rildgenofile, mapfile)
 }
 
 
+writefiles = function(mapwheat, founder, ril, phenodata, founderraw, pedidata, mapdata)
+{
+  
+  write.table(mapwheat,file='map.wheat.txt', sep='\t', 
+              quote = FALSE, row.names = FALSE);
+  write.table(founder, file='founderfile.txt', sep='\t');
+  write.table(ril, file='finalfile.txt', sep='\t');
+  
+  write.table(phenodata, file='wheat.phenotype', sep='\t', quote = FALSE);
+  write.table(founderraw, file='founders.geno2.txt', sep=' ', quote = FALSE, row.names = FALSE);
+  write.table(pedidata, file='pedfile.txt', sep='\t');
+  write.table(mapdata, file='mapfile.txt', sep='\t');
+}
+
+create_pedigree = function(peddata, rildata)
+{
+  
+  y = rildata[1:2,3:722];
+  y = t(y)
+  rownames(y) = 1:nrow(y);
+  yy = merge(y,peddata, by.x='2', by.y='id');
+  colnames(yy)[1:2] = c('Marker', 'id' );
+  return(yy);
+}
 
 
 
-f = get_MAGIC('wheat_pheno.csv', 'founders.geno.csv','wheat_geno.csv','wheat_geno_coordinates.csv'); 
+transform_wheatgeno = function()
+{
+  
+  wg2 = read.table('wheat_geno2.csv', sep=',', header=F);
+  wg2a = wg2[, -c(1,2,4:11)];
+  clr = ncol(rils_raw);
+  clr1 = ncol(wg2a);
+  n = as.vector(rils_raw[1, 3:(clr)]);
+  n = unlist(t(n));
+  
+  genotypename = as.vector(rils_raw[2, 3:(clr)]);
+  genotypename = unlist(t(genotypename));
+  
+  genotypename1 = as.vector(wg2a[1, 2:(clr1)]);
+  genotypename1 = unlist(t(genotypename1));
+  
+  markername = as.character(wg2a[2:nrow(wg2a), 1]);
+  
+  
+  i = match(genotypename1, genotypename);
+  mn = c('',n[i]);
+  colname = colnames(rils_raw)[3:(clr)]
+  cn = c('',colname[i])
+  
+  R <- apply(wg2a[2:18602, 2:644],2, convert.snp);
+  
+  markers = cbind(markername,R);
+  m = rbind(cn, mn, c('',genotypename1), markers);
+  
+  write.table(m, file='wheat_pheno_alt.csv', sep=',', quote=F, row.names=F)
+}
 
-t = createQTLTable(f$map_raw);
-write.table(t[,c(2,1,9,10,7,8)], file='qtltable.csv', sep=',', row.names = F);
+
+#f = get_MAGIC('wheat_pheno.csv', 'founders.geno.csv','wheat_geno.csv','wheat_geno_coordinates.csv'); 
+phenofile = 'wheat_pheno.csv';
+foundersgenofile = 'founders.geno.csv';
+rildgenofile = 'wheat_geno.csv';
+mapfile = 'wheat_geno_coordinates.csv';
+
+
+pheno_raw = read.table(phenofile, header=TRUE, sep=',');
+# foundersdata
+founders_raw = read.table(foundersgenofile, header=T, sep=',');
+#rilsdata
+rils_raw = read.table(rildgenofile, header=T, sep=',');
+# map
+map_raw = read.table(mapfile, sep=',', header=TRUE);
+# pedigree
+pedigree_raw = read.table('pedigree_table.csv',sep=',', header=TRUE);
+
+parents = process_founders(founders_raw, map_raw, 'X13074');
+rils = process_rils(rils_raw, map_raw);
+phenodata = create_phenodata(pheno_raw, rils_raw);
+peddata = create_pedigree(pedigree_raw, rils_raw);
+
+peddata$id = as.numeric(peddata$id);
+peddata$mother = as.numeric(peddata$mother);
+peddata$father = as.numeric(peddata$father);
+k = match(parents$mapfounder$Marker, colnames(rils$rildataH)[2:ncol(rils$rildataH)]);
+break();
+writefiles(parents$mapfounder[,c('Marker', 'chromosome', 'cM' )], parents$mapfounder[,c(1,5:12)],
+                      rils$rildataH[,c(1,k+1)], phenodata, founders_raw, peddata[,2:4], map_raw[,c(1,3,2)]);
+
+
+# Check for population structure
+p = check_PStruct(t(rils$R));
+png('PCA_PS.png', width = 1600, height = 1600,res=200);
+plot(lambda/sum(lambda),ylab="Fraction Explained")
+dev.off()
+
+# Create mpcrop object
+#cross = read.mpcross(founderfile='founderfile1.txt', finalfile='finalfile.txt', 
+                     #pedfile='pedfile.txt', mapfile='mapfile.txt', phenofile = 'wheat.phenotype');
+
